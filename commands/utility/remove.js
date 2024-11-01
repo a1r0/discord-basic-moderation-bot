c1a1decae438ee161fba63fdc9bd81f44082e596
Alexey <alexsem132@gmail.com>
2024-11-01 03:44:47 +0000

    Updated internal text of the removal command

--- commands/utility/remove.js
+++ commands/utility/remove.js
@@ -3,13 +3,13 @@ const { SlashCommandBuilder, Colors } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('remove')
-		.setDescription('Moves a message to a specific channel.')
+		.setDescription('Removes a specific amount of the messages in the channel.')
 		.addStringOption(option => 
 			option.setName('amount')
 				.setDescription('Amount of messages that needed to be moved')
 				.setRequired(true)),
 	async execute(interaction) {
-        const amount = interaction.options.getString('amount')
+        const amount = await interaction.options.getString('amount')
         const messages = await interaction.channel.messages.fetch({limit: amount})
         const messagesArray = Array.from(messages.values()).reverse();
 
@@ -20,7 +20,7 @@ module.exports = {
 				await msg.delete();
             }
 
-			await interaction.editReply('ℹ️' + `${messagesArray.length} ` +'Messages were removed')
+			await interaction.editReply('ℹ️ ' + `${messagesArray.length} ` +'Messages were removed')
             
 		} catch (error) {
 			console.error(error);
